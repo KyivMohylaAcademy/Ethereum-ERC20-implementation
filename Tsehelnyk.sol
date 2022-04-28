79c77855f606172851ec0c50052bb0a535a54019
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2022-04-28 13:24:59 +0300

    Create token contract

---
+++ Tsehelnyk.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/24a0bc23cfe3fbc76f8f2510b78af1e948ae6651/contracts/utils/math/SafeMath.sol";
+
+contract TSH is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Tsehelnyk", "TSH") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    receive() external payable
+    {
+        _mint(msg.sender, SafeMath.mul(msg.value, 10));
+    }
+}
