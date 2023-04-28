6ada60ebe9020551669306c32bd3878373e7f3e5
yesipovaD <107425989+yesipovaD@users.noreply.github.com>
2023-04-28 21:12:51 +0300

    Yesypova Daria

---
+++ transaction.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Yesypova is ERC20 {
+    using SafeMath for uint256;
+    uint256  initialSupply = 1000000 * (10 ** 18);
+    constructor() ERC20("Yesypova", "YES") {
+        _mint(address(this), initialSupply);
+    }
+
+    receive() external payable {
+        require(msg.value > 0, "Amount must be greater then 0");
+        uint256 tokenAmountToSend = SafeMath.mul(msg.value, 10);
+        _transfer(address(this), msg.sender, tokenAmountToSend);
+    }
+}
